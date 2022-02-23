55b1212343693b5591f63fc9b842201cad047368
Timo Pontinen <toponen@gmail.com>
2022-02-23 14:56:34 +0200

    some version

--- curl_test.c
+++ curl_test.c
@@ -2,15 +2,41 @@
 #include <string.h>
 
 
-int	wcontains(char *word, char letter)
+int	wcontains(char *word, char letter, int *w_location)
 {
-	while (*word)
+	int i = 0;
+
+	while (word[i])
 	{
-		if (*word == letter)
+		if (*w_location == i)
+		{
+			w_location++;
+			i++;
+		}
+		else if (word[i] == letter)
 			return (1);
-		word++;
+		else
+			i++;
 	}
 	return (0);
+}	
+
+int exclude_char(char *cha, char *word)
+{
+	int i = 0;
+	int c;
+	while (word[i])
+	{
+		c = 0;
+		while (cha[c])
+		{
+			if(word[i] == cha[c])
+				return (0);
+			c++;
+		}
+		i++;
+	}
+	return (1);
 }
 
 int	main()
@@ -20,10 +46,13 @@ int	main()
 	size_t	linecap = 5;
 	ssize_t	line_len;
 	file = fopen("wlist", "r");
+	int e[] = {1,2,3};
+	int r[] = {2,4};
+	char *exc = "unmsaibc";
 
 	while ((line_len = getline(&word, &linecap, file)) > 0)
 	{
-		if (word[0] == 't' && wcontains(word,'e') && wcontains(word,'r'))
+		if (exclude_char(exc, word) && word[1] == 'r' &&  word[0] == 't' && wcontains(word,'e',e) && wcontains(word,'r',r))
 			printf("%s", word);
 	}
 
